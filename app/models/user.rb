c86961ede15c9db2531f5bf760f776890a053109
Vinicius Libero <libero.moreira@gmail.com>
2024-03-10 18:29:57 -0300

    edited user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+
+  # has_one :profile
+  # has_one :account
+  # has_many :feedbacks
+  # has_many :messages
+  # has_many :events
+  # has_many :lodgings
+  # has_many :products
+  # has_many :services
+  # has_many :jobs
 end
