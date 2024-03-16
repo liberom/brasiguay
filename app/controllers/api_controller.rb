3152135df4440c58220d0de3833246b968ec90a1
Vinicius Libero <libero.moreira@gmail.com>
2024-03-16 20:12:01 -0300

    several changes

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,7 +2,8 @@ class ApiController < ActionController::Base
 
   def i18n
     # locale param was defined in config/routes.rb
-    render file: "app/assets/i18n/" + params['locale']
+
+    render file: "#{Rails.root.join('app', 'assets', 'i18n', params['locale'])}"
   end
 
   def datatable
