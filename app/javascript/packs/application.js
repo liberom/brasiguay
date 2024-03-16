662940878ee8b94c6924cfd31c44272350bdd9bb
Vinicius Libero <libero.moreira@gmail.com>
2024-03-16 15:39:23 -0300

    installed ActionText

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -22,3 +22,6 @@ document.addEventListener('DOMContentLoaded', appInit);
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+require("trix")
+require("@rails/actiontext")
