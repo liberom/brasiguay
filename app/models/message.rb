8cc583ddd7cfd18662a95b212bd17f1a31926150
Vinicius Libero <libero.moreira@gmail.com>
2024-03-16 18:25:21 -0300

    added a comment

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,9 @@ class Message < ApplicationRecord
   belongs_to :sender, class_name: 'User', foreign_key: 'sender_id'
   belongs_to :receiver, class_name: 'User', foreign_key: 'receiver_id'
   has_rich_text :content
+
+  # enum status: {
+  #   unread: 0,
+  #   read: 1
+  # }
 end
